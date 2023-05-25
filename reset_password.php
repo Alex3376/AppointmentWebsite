fcd4d7fcc1b0873680642131c16c09ca56af6b19
Matthew Hobbick <103011039+Alex3376@users.noreply.github.com>
2023-05-24 22:01:33 -0700

    Update reset_password.php

--- reset_password.php
+++ reset_password.php
@@ -1,41 +1,27 @@
 <?php
 
-  if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-      die("Valid email is required");
-  }
-  
-  if (isset($_POST['email'])) {
-      require_once('database.php');
-      
-      $email = $mysqli->real_escape_string($_POST["email"]);
-      
-      $sql = $mysqli->query("SELECT id FROM user WHERE email='$email'");
-      if ($sql->num_rows > 0) {
-          
-          $token = "poitzgdjfkmwfhbughcbdjbfvqz1234567890";
-          $token = str_shuffle($token);
-          $token = sub($token, 0, 10);
-          
-          $mysqli->query("UPDATE user SET token='$token', "
-                  . "tokenExpire=DATE_ADD(NOW(), INTERVAL 5 MINUTE
-                  WHERE email ='$email'");
-          
-          exit('please check your email inbox');
-      } else
-          exit('please check your inputs');
-  }
+require_once "functions.php";
+
+if (isset($_GET['email']) && isset($_GET['token'])) {
+    require_once('database.php');
+
+    $email = $mysqli->real_escape_string($_GET['email']);
+    $token = $mysqli->real_escape_string($_GET['token']);
+
+    $sql = $mysqli->query("SELECT id FROM user WHERE
+			email='$email' AND token='$token' AND token<>'' AND tokenExpire > NOW()");
+
+    if ($sql->num_rows > 0) {
+        $newPassword = generateNewString();
+        $newPasswordEncrypted = password_hash($newPassword, PASSWORD_BCRYPT);
+        $mysqli->query("UPDATE user SET token='', password_hash = '$newPasswordEncrypted'
+				WHERE email='$email'
+			");
+
+        echo "Your New Password Is $newPassword<br><a href='login.php'>Click Here To Log In</a>";
+    } else
+        redirectToLoginPage();
+} else {
+    redirectToLoginPage();
+}
 ?>
-<html>
-<head>
-    <title>Login</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
-</head>
-<body>
-    <form method="post">
-        <label for="email">Reset your password</label>
-        <input type="email" name="email" id="email">
-        <button>Log in</button>
-    </form>
-</body>
-</html>
